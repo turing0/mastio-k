fe3d808e2a65faee223ca39ca05f62eaa2a6d4b2
turing0 <turing175@gmail.com>
2023-06-03 17:39:03 +0800

    fix build

--- app/[server]/[username]/page.tsx
+++ app/[server]/[username]/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Header from "@/app/components/Header";
-import PostFeed from "@/app/components/posts/PostFeed";
-import UserBio from "@/app/components/users/UserBio";
-import UserHero from "@/app/components/users/UserHero";
-import useUser from "@/app/hooks/useUser";
+// import PostFeed from "@/app/components/posts/PostFeed";
+// import UserBio from "@/app/components/users/UserBio";
+// import UserHero from "@/app/components/users/UserHero";
+// import useUser from "@/app/hooks/useUser";
 import { ClipLoader } from "react-spinners";
 
 interface UserViewParams {
@@ -19,23 +19,23 @@ const UserView = ({ params }: { params: UserViewParams }) => {
     // console.log(server, username);
     // const { userId } = router.query;
   
-    const { data: fetchedUser, isLoading } = useUser(server, username);
+    // const { data: fetchedUser, isLoading } = useUser(server, username);
 
     
-    if (isLoading || !fetchedUser) {
-      return (
-        <div className="flex justify-center items-center h-full">
-          <ClipLoader color="lightblue" size={80} />
-        </div>
-      )
-    }
+    // if (isLoading || !fetchedUser) {
+    //   return (
+    //     <div className="flex justify-center items-center h-full">
+    //       <ClipLoader color="lightblue" size={80} />
+    //     </div>
+    //   )
+    // }
   
     return (
       <>
-        <Header showBackArrow label={fetchedUser?.display_name} />
-        <UserHero server={server as string} username={username as string} />
+        {/* <Header showBackArrow title={fetchedUser?.display_name} /> */}
+        {/* <UserHero server={server as string} username={username as string} />
         <UserBio server={server as string} userId={username as string} />
-        <PostFeed server={server as string} userId={fetchedUser?.id as string} />
+        <PostFeed server={server as string} userId={fetchedUser?.id as string} /> */}
       </>
      );
   }
