22ecb8143858c78c007a6355202f4310154fab20
turing0 <turing175@gmail.com>
2023-06-03 21:47:58 +0800

    image

--- app/[server]/[username]/[statusId]/page.tsx
+++ app/[server]/[username]/[statusId]/page.tsx
@@ -44,7 +44,7 @@ const StatusView = ({ params }: { params: StatusViewParams }) => {
         <ul className="[&_p:last-child]:text-slate-500 [&_p:first-child]:text-lg divide-y divide-slate-200">
           <li className="p-4">
             <BigPost server={server} statusId={statusId} data={fetchedPost}>
-              {fetchedPost?.media_attachments?.length > 0 && (
+              {/* {fetchedPost?.media_attachments?.length > 0 && (
 								<img
 									// fill={true}
 									style={{ objectFit: 'cover' }}
@@ -52,7 +52,37 @@ const StatusView = ({ params }: { params: StatusViewParams }) => {
 									src={`${fetchedPost?.media_attachments[0].url}`}
 									alt=""
 								/>
-								)}
+              )} */}
+
+<div style={{ 
+  display: 'flex', 
+  flexWrap: 'wrap', 
+  justifyContent: 'space-between' 
+}}>
+  {
+    fetchedPost?.media_attachments?.length > 0 && fetchedPost?.media_attachments.map((attachment: { preview_url: string }, index: number) => (
+      <div style={{ 
+        flexBasis: fetchedPost?.media_attachments.length === 1 ? '100%' : 'calc(50% - 10px)',  // 如果只有一张图片，占100%宽度，否则占50%宽度减去左右间距
+        margin: '5px',  // 上下左右的间距都为5px
+        height: fetchedPost?.media_attachments.length === 1 ? '600px' : '300px',  // 如果只有一张图片，高度为600px，否则为300px
+        overflow: 'hidden'  // 如果图片超出容器，将其隐藏
+      }}>
+        <img
+          key={index}
+          style={{ 
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%'
+          }}
+          className="rounded-3xl"
+          src={attachment.preview_url}
+          alt={`Attachment ${index + 1}`}
+        />
+      </div>
+    ))
+  }
+</div>
+              
             </BigPost>
           </li>
 			  </ul>
--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -96,23 +96,35 @@ const Feed: React.FC<PostFeedProps> = ({ server, userId, type }) => {
 								data={post}
 								server={server}
 							>
-								{/* {image} */}
-								{post?.media_attachments?.length > 0 && (
-								// <Image
-								// 	fill={true}
-								// 	style={{ objectFit: 'cover' }}
-								// 	className="rounded-3xl"
-								// 	src={`${post?.media_attachments[0].url}`}
-								// 	alt="Gradient"
-								// />
-								<img
-									// fill={true}
-									style={{ objectFit: 'cover' }}
-									className="rounded-3xl"
-									src={`${post?.media_attachments[0].preview_url}`}
-									alt="Gradient"
-								/>
-								)}
+<div style={{ 
+  display: 'flex', 
+  flexWrap: 'wrap', 
+  justifyContent: 'space-between' 
+}}>
+  {
+    post?.media_attachments?.length > 0 && post?.media_attachments.map((attachment: { preview_url: string }, index: number) => (
+      <div style={{ 
+        flexBasis: post?.media_attachments.length === 1 ? '100%' : 'calc(50% - 10px)',  // 如果只有一张图片，占100%宽度，否则占50%宽度减去左右间距
+        margin: '5px',  // 上下左右的间距都为5px
+        height: post?.media_attachments.length === 1 ? '400px' : '300px',  // 如果只有一张图片，高度为600px，否则为300px
+        overflow: 'hidden'  // 如果图片超出容器，将其隐藏
+      }}>
+        <img
+          key={index}
+          style={{ 
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%'
+          }}
+          className="rounded-3xl"
+          src={attachment.preview_url}
+          alt={`Attachment ${index + 1}`}
+        />
+      </div>
+    ))
+  }
+</div>
+
 							</Post>
 						</li>
 					),
